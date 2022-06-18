66f01d92696ab7f15d7fd6df13daf2c668bf749d
vukhanh09 <vukhanh0920@gmail.com>
2022-06-18 21:56:59 +0700

    update

--- src/screen/createJob.js
+++ src/screen/createJob.js
@@ -12,24 +12,53 @@ function CreateETLjob() {
         alias:  ''
       },
     ])
+    const [info, setInfo] = useState([
+      { 
+        DagId: '', 
+        Schedule: '',
+        owner:  '',
+        tags_name:  ''
+      },
+    ])
 
-    const [query, setQuery] = useState({})
-    // const [data, setData] = useState()
+    const [formSrcQuery, setFormSrcQuery] = useState([
+      { 
+        queryname: '', 
+        querydetail: '',
+        targettype:  '',
+        connectstring: '',
+        targettable:  ''
+      },
+    ])
+    console.log(formSrcQuery)
 
-    const [target, setTarget] = useState({})
+
   
     const handleFormSrcChange = (event, index) => {
       let data = [...formSrcFields] ;
       data[index][event.target.name] = event.target.value;
       setFormSrcFields(data);
     }
+    const handleFormSrcQuery = (event, index) => {
+      let data = [...formSrcQuery] ;
+      data[index][event.target.id] = event.target.value;
+      setFormSrcQuery(data);
+    }
   
     const submit = (e) => {
       e.preventDefault();
       let data = {
-        'source':formSrcFields,
-        'query':[query],
-        'target':[target]
+        'Job':[
+          {
+            'DagId':info.DagId,
+            "Schedule":info.Schedule,
+            "owner":info.owner,
+            'tags':info.tags_name,
+            'source':formSrcQuery,
+            'query':formSrcQuery
+          }
+
+        ]
       }
 
       console.log(data)
@@ -51,108 +80,184 @@ function CreateETLjob() {
       data.splice(index, 1)
       setFormSrcFields(data)
     }
+    const removeQuery = (index) => {
+      let data = [...formSrcQuery];
+      data.splice(index, 1)
+      setFormSrcQuery(data)
+    }
+
+    const addFieldQuery = () =>{
+      let object = {
+        queryname: '',
+        querydetail: '',
+        targettype:  '',
+        connectstring:  '',
+        targettable: ''
+      }
+  
+      setFormSrcQuery([...formSrcQuery, object])
+
+    }
 
 
 
   
     return (
       <div className={styles.main}>
-        <div className={styles.content}>
-            <Box 
-                component="form"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                // autoComplete="off"
-                >
-                <div>
-                    <TextField
-                        label="Querry Name"
-                        id="querryname"
-                        value={query.querryname}
-                        size="small"
-                        onChange={e=>setQuery({...query,'querryname':e.target.value})}
-                        // onChange={e=>setQuery(e.target.value)}
-                        />
+        <div  className={styles.info}>
+          <Box
+            // className={styles.box}
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+            >
+            <div>
+                <TextField
+                    label="DagId"
+                    id="DagId"
+                    value={info.DagId}
+                    size="small"
+                    onChange={event => setInfo({...info,'DagId':event.target.value})}
+                    // onChange={e=>setQuery(e.target.value)}
+                    />
 
-                        
-                        <TextField
-                        label="Querry Detail"
-                        id="querrydetail"
-                        size="small"
-                        value={query.querrydetail}
-                        onChange={e=>setQuery({...query,'querrydetail':e.target.value})}
+                    
+                    <TextField
+                    label="Schedule"
+                    id="Schedule"
+                    size="small"
+                    value={info.Schedule}
+                    onChange={event => setInfo({...info,'Schedule':event.target.value})}
                     />
-                </div>
-                <div>
-                    <TextField
-                        label="Target type"
-                        id="targettype"
-                        value={target.targettype}
-                        onChange={e=>setTarget({...target,'targettype':e.target.value})}
-                        size="small"
-                        />
-                    <TextField
-                        label="Connect string"
-                        id="connectstring"
-                        value={target.connectstring}
-                        onChange={e=>setTarget({...target,'connectstring':e.target.value})}
-                        size="small"
+
+                <TextField
+                    label="Owner"
+                    id="owner"
+                    value={info.owner}
+                    onChange={event =>setInfo({...info,'owner':event.target.value})}
+                    size="small"
                     />
-                    <TextField
-                        label="Table name"
-                        id="tablename"
-                        value={target.tablename}
-                        onChange={e=>setTarget({...target,'tablename':e.target.value})}
-                        size="small"
-                        />
-                </div>
+                <TextField
+                    label="Tags name"
+                    id="tags_name"
+                    value={info.tags_name}
+                    onChange={event =>setInfo({...info,'tags_name':event.target.value})}
+                    size="small"
+                />
+            </div>
                 
-            </Box>
-            <form onSubmit={submit}>
-            {formSrcFields.map((form, index) => {
-                return (
-                <div key={index}>
-                    {/* <input
-                    name='sourcetype'
-                    placeholder='Loại Nguồn'
-                    onChange={event => handleFormSrcChange(event, index)}
-                    value={form.sourcetype}
-                    /> */}
-                    <select name='sourcetype'  value={form.sourcetype} onChange={event => handleFormSrcChange(event, index)}>
-                        <option value="0">Chọn nguồn</option>
-                        <option value="storage">Lưu trữ đám mây</option>
-                        <option value="dwh">Kho dữ liệu</option>
-                        <option value="bigdata">Dữ liệu lớn</option>
-                    </select>
-                    
-                    
-                    <input
-                    name='connectstring'
-                    placeholder='storage path/tên database'
-                    onChange={event => handleFormSrcChange(event, index)}
-                    value={form.connectstring}
-                    />
-                    <input
-                    name='tablename'
-                    placeholder='Tên bảng/tên file'
-                    onChange={event => handleFormSrcChange(event, index)}
-                    value={form.tablename}
-                    />
-                    <input
-                    name='alias'
-                    placeholder='Tên bảng tạm'
-                    onChange={event => handleFormSrcChange(event, index)}
-                    value={form.alias}
-                    />
-                    <button onClick={() => removeFields(index)}>Remove</button>
-                </div>
-                )
-            })}
-            </form>
+        </Box>
+
         </div>
-        <button onClick={addFields}>Thêm Nguồn..</button>
+        <div className={styles.content}>
+            <form onSubmit={submit}>
+              {formSrcFields.map((form, index) => {
+                  return (
+                  <div key={index}>
+                      {/* <input
+                      name='sourcetype'
+                      placeholder='Loại Nguồn'
+                      onChange={event => handleFormSrcChange(event, index)}
+                      value={form.sourcetype}
+                      /> */}
+                      <select name='sourcetype'  value={form.sourcetype} onChange={event => handleFormSrcChange(event, index)}>
+                          <option value="0">Chọn nguồn</option>
+                          <option value="storage">Lưu trữ đám mây</option>
+                          <option value="dwh">Kho dữ liệu</option>
+                          <option value="bigdata">Dữ liệu lớn</option>
+                      </select>
+                      
+                      
+                      <input
+                      name='connectstring'
+                      placeholder='storage path/tên database'
+                      onChange={event => handleFormSrcChange(event, index)}
+                      value={form.connectstring}
+                      />
+                      <input
+                      name='tablename'
+                      placeholder='Tên bảng/tên file'
+                      onChange={event => handleFormSrcChange(event, index)}
+                      value={form.tablename}
+                      />
+                      <input
+                      name='alias'
+                      placeholder='Tên bảng tạm'
+                      onChange={event => handleFormSrcChange(event, index)}
+                      value={form.alias}
+                      />
+                      <button onClick={() => removeFields(index)}>Remove</button>
+                  </div>
+                  )
+              })}
+                      <button onClick={addFields}>Thêm Nguồn..</button>
+            </form>
+            <div  className={styles.elm2}>
+              {/* <input placeholder='Số query' onChange={e => addFieldQuery(e.target.value)}/> */}
+              {formSrcQuery.map((form, index) =>(
+                <Box key={index}
+                // className={styles.box}
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                    >
+                    <div>
+                        <strong>Query {index}</strong>
+                        <TextField
+                            label="Querry Name"
+                            id="querryname"
+                            value={form.querryname}
+                            size="small"
+                            onChange={event => handleFormSrcQuery(event, index)}
+                            // onChange={e=>setQuery(e.target.value)}
+                            />
+
+                            
+                            <TextField
+                            label="Querry Detail"
+                            id="querrydetail"
+                            size="small"
+                            value={form.querrydetail}
+                            onChange={event => handleFormSrcQuery(event, index)}
+                            />
+                            <button onClick={() => removeQuery(index)}>Remove</button>
+                    </div>
+                    <div>
+                        <TextField
+                            label="Target type"
+                            id="targettype"
+                            value={form.targettype}
+                            onChange={event => handleFormSrcQuery(event, index)}
+                            size="small"
+                            />
+                        <TextField
+                            label="Connect string"
+                            id="connectstring"
+                            value={form.connectstring}
+                            onChange={event => handleFormSrcQuery(event, index)}
+                            size="small"
+                        />
+                        <TextField
+                            label="Table name"
+                            id="tablename"
+                            value={form.tablename}
+                            onChange={event => handleFormSrcQuery(event, index)}
+                            size="small"
+                            />
+                    </div>
+                    
+                </Box>
+              ))
+              }
+                <button onClick={addFieldQuery}>Thêm Query..</button>
+          </div>
+        </div>
         <br />
         <button onClick={submit}>Tạo tiến trình</button>
       </div>
